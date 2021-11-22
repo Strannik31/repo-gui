6aeeb6d5c58371ade45b7ae3a96d195a10696d1e
Strannik31 <93123379+Strannik31@users.noreply.github.com>
2021-11-22 10:42:18 +0300

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,6 @@
+def num_translate(i):
+    my_dict = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+    return my_dict.get(i)
+
+
+print(num_translate('zero'))
